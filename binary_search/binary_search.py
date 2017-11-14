527757b79c3d04031dcda24fda864486e75c3094
DwoaC <jason.jennette@gmail.com>
2017-11-14 08:42:23 +0000

    Updating Docs

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,25 +1,3 @@
-'''
-I'm a big fan of [CodeInGame.com](https://www.codingame.com) for practicing interesting
-development problems.  One particular problem, [Shadows Of The Knight](https://www.codingame.com/training/medium/shadows-of-the-knight-episode-1),
-is a binary search problem.  The key in the problem is that there are 2 binary searches
-happening at the same time, one in the x direction and one in the y direction.
-
-I thought this was a great opportunity to play with a generator that makes use of both
-sides of the yield statement.
-
-Random points:
-* I hate sending in strings as parameters to change behaviour.  Given how the game
-is implemented I think this is sensible in this context.  The sent values could have
-been changed to 1, -1 and 0.  If we uses constants to represent these as high, low and
-null it would be just as readable as the strings.
-* There is a lot to be said for the search raising a StopIteration exception
-when we reach a final value.  Again it fit easier with the game to just
-let the searcher keep repeating the last value.  This way the x and y searches
-could continue on operating in parallel.
-* Dito for raising an exception for sending in values other than 'high', 'low' and
-None
-'''
-
 import logging
 
 logging.basicConfig()
